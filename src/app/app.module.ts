b500c6e8c695a0a368236209b5fdea2b38663e3a
James <jamespatterson1985@yahoo.com>
2022-10-15 12:05:34 -0400

    rxjs: add in mat icons and mat buttons to be used in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent, MainComponent, ToolbarComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
